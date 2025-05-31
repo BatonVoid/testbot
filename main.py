8c881f31989b7fa08b19ea3d262c255a40cacbe1
BatonVoid <akkanat.kz@mail.ru>
2025-05-31 13:40:50 +0500

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from datetime import datetime, timedelta
 from sqlalchemy import create_engine, Column, Integer, String, BigInteger, Boolean, JSON
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from telegram.ext import DefaultBotProperties
 import random
 import logging
 import json
